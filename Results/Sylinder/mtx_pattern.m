9c3bcb26e89d02f38cfc37a9fe407ffbf3117531
William Liao <r05246012@ntu.edu.tw>
2020-09-10 19:38:22 +0800

    update matlab test codes

--- Results/Sylinder/mtx_pattern.m
+++ Results/Sylinder/mtx_pattern.m
@@ -17,6 +17,14 @@ spy(M(1:100,1:100),'r');
 figure(4);
 spy(M,'r');
 
+b1 = 214;
+b2 = 2888;
+D1 = D(:,b1+1:b2-1);
+D1t = D1.';
 a = Dt*M*D;
 figure(5);
 spy(a,'r');
+figure(6);
+spy(D1,'r');
+figure(7)
+spy(D1t,'r');
--- Results/Sylinder/solvequad.m
+++ Results/Sylinder/solvequad.m
@@ -21,9 +21,47 @@ a = Dt*M*D;
 %figure(5);
 %spy(a,'r');
 
+% get lower bounds
+mtx_name = 'invKappa_TV.mtx';
+invKappa = mmread(mtx_name);
+mtx_name = 'lb0_TV.mtx';
+lb0 = mmread(mtx_name);
+mtx_name = 'lb_TV.mtx';
+lb = mmread(mtx_name);
+mtx_name = 'biFlag_TV.mtx';
+biFlag = mmread(mtx_name);
+
 n = length(a);
 b = rand(n,1);
-all(b>0)
+b1 = 214;
+b2 = 2888;
+
+if all(b>0)
+    fprintf("b>0: TRUE\n");
+else
+    fprintf("b>0: FALSE\n");
+end
+
+xb = zeros(n,1);
+xb(1:b1) = -inf;
+xb(b2:end) = -inf;
 options = optimoptions('quadprog','Display','iter','Algorithm','interior-point-convex');
 x = quadprog(a,b,[],[],[],[],zeros(n,1),inf(n,1),[],options);
-all(x>=0)
+if all(x>=0)
+    fprintf("x>=0: TRUE\n");
+else
+    fprintf("x>=0: FALSE\n");
+end
+
+% cvx toolbox
+H = (a+a')/2;
+cvx_begin
+    cvx_solver sedumi
+    variable x(n)
+    minimize ( (1/2)*quad_form(x,H) + b'*x)
+    x >= 0;
+cvx_end
+
+% nonnegative least square solvers
+H = (a+a')/2;
+[R,flag] = chol(H);
