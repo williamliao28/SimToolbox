615ff5c5026c5ccbf462aae47fcd04c01e0078df
William Liao <b00201028.ntu@gmail.com>
2020-10-22 21:12:22 +0800

    test modification

--- Results/Sylinder/flopstest_main.m
+++ Results/Sylinder/flopstest_main.m
@@ -6,12 +6,12 @@ xb = zeros(n,1);
 options = optimoptions('quadprog','Display','iter','Algorithm','interior-point-convex');
 
 profile on
-%quadprog(a,b,[],[],[],[],xb,inf(n,1),[],options);
-lsqnonneg(A1,y);
+quadprog(a,b,[],[],[],[],xb,inf(n,1),[],options);
+%lsqnonneg(A1,y);
 profileStruct = profile('info');
-[flopTotal,Details]  = FLOPS('lsqnonneg','flopstestMAT',profileStruct);
+[flopTotal,Details]  = FLOPS('quadprog','flopstestMAT',profileStruct);
 
-fprintf("Total number of flops: %lf\n",flopTotal);
+fprintf("Total number of flops: %d\n",flopTotal);
 Details
 
 profile off
