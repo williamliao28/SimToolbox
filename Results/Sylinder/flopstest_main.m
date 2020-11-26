2e91c66c2770a3152b8088c84337b31bc468a8c1
William Liao <r05246012@ntu.edu.tw>
2020-11-26 22:58:46 +0800

    fix code

--- Results/Sylinder/flopstest_main.m
+++ Results/Sylinder/flopstest_main.m
@@ -5,6 +5,7 @@ load testvars
 xb = zeros(n,1);
 options = optimoptions('quadprog','Display','iter','Algorithm','interior-point-convex');
 
+profile clear
 profile on
 %quadprog(a,b,[],[],[],[],xb,inf(n,1),[],options);
 lsqnonneg(A1,y);
